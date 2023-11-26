db1cd3a447d0ceb1bae5fc478657b73345930abb
julian <julianignaciocarelli@gmail.com>
2023-11-26 17:21:40 -0300

    feat(docker): add docker to create a container

--- vite.config.ts
+++ vite.config.ts
@@ -24,7 +24,11 @@ export default defineConfig({
     }),
   ],
   server: {
+    host: true,
     port: 3000,
+    watch: {
+      usePolling: true,
+    },
   },
   build: {
     modulePreload: false,
